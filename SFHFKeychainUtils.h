96dba4c1be44be564637ee532573f9c77cd74698
Sam Yang <yangch.work@gmail.com>
2014-10-22 19:14:26 +0800

    update

--- SFHFKeychainUtils.h
+++ SFHFKeychainUtils.h
@@ -29,6 +29,7 @@
 // Updated with ARC support.  Originally forked from:
 // https://github.com/beelsebob/F1Pad/commit/66ea68e08eef0a66c706c45583e071f0a032f3c8
 
+#import <Foundation/Foundation.h>
 
 @interface SFHFKeychainUtils : NSObject
 
